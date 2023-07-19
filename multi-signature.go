22c38db3b98f68bbd19303d0bf66aa2658c97b16
Amir <amirranjbar.dev@gmail.com>
2023-07-19 13:01:10 +0330

    update CreateAndBroadcastMultiTransaction

--- multi-signature.go
+++ multi-signature.go
@@ -2,23 +2,39 @@ package tronWallet
 
 import (
 	"crypto/ecdsa"
+	"errors"
 
 	"github.com/ranjbar-dev/tron-wallet/enums"
+	"github.com/ranjbar-dev/tron-wallet/grpcClient"
 )
 
-func CreateAndBroadcastMultiTransaction(node enums.Node, fromAddressBase58 string, toAddressBase58 string, amountInSun int64, privateKeys []*ecdsa.PrivateKey, _ ecdsa.PrivateKey) error {
+func CreateAndBroadcastMultiTransaction(node enums.Node, fromAddressBase58 string, toAddressBase58 string, amountInSun int64, privateKeys []*ecdsa.PrivateKey, _ ecdsa.PrivateKey) (string, error) {
 
 	transaction, err := createTransactionInput(node, fromAddressBase58, toAddressBase58, amountInSun)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	for _, privateKey := range privateKeys {
 		transaction, err = signTransaction(transaction, privateKey)
 		if err != nil {
-			return err
+			return "", err
 		}
 	}
 
-	return broadcastTransaction(node, transaction)
+	c, err := grpcClient.GetGrpcClient(node)
+	if err != nil {
+		return "", err
+	}
+
+	res, err := c.Broadcast(transaction.Transaction)
+	if err != nil {
+		return "", err
+	}
+
+	if !res.Result {
+		return "", errors.New(res.Code.String())
+	}
+
+	return string(res.Message), nil
 }
